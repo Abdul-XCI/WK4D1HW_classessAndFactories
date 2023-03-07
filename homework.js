d73392a35c1d07c7cb91405a74fca10e810ca55e
Abdul R <abdul.kasmo.iv@outlook.com>
2023-03-08 00:18:07 +0100

    created person class

--- homework.js
+++ homework.js
@@ -23,3 +23,69 @@ class Hamster {
         return this.price;
     }
 }
+
+// Person class
+class Person 
+{
+    constructor(name)
+    {
+        this.name = name;
+
+        age = 0;
+        height = 0;
+        weight = 0;
+        mood = 0;
+        hamsters = [];
+        bankAccount = 0;
+    }
+
+    getName()
+    {
+        return this.name;
+    }
+
+    getAge()
+    {
+        return age;
+    }
+
+    getWeight()
+    {
+        return weight;
+    }
+
+    greet(personName)
+    {
+        console.log(`Hello ${personName}.`);
+    }
+
+    eat()
+    {
+        weight++;
+        modd++;
+    }
+
+    exercise()
+    {
+        weight--;
+
+    }
+
+    ageUp()
+    {
+        age++;
+        height++
+        weight++;
+        mood--;
+        bankAccount+= 10;
+    }
+
+    buyHamster(hamsterObj)
+    {
+        hamsters.push(hamsterObj);
+        mood += 10;
+        bankAccount -= hamsterObj.getPrice();
+
+    }
+
+}
